b26950efc89c16e7fb3f891ca1e116c34eb7b69e
BarbaraHernandez <bandezanne@gmail.com>
2019-08-05 22:55:36 -0400

    added event listeners to all buttons, currently conflict with firebase data structure

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,34 +24,104 @@ $(document).ready(function(){
     var yellowPoints = 0;
 
     //get current point values
-    var redRef = database.ref('housePoints');
-    redRef.on('value', function(snapshot) {
+    var pointsRef = database.ref();
+    pointsRef.on('value', function(snapshot) {
+        console.log(snapshot.val());
         $("#red-points").text(snapshot.val().redHousePoints);
         redPoints = snapshot.val().redHousePoints;
+        $("#blue-points").text(snapshot.val().blueHousePoints);
+        bluePoints = snapshot.val().blueHousePoints;
+        $("#green-points").text(snapshot.val().greenHousePoints);
+        greenPoints = snapshot.val().greenHousePoints;
+        $("#yellow-points").text(snapshot.val().yellowHousePoints);
+        yellowPoints= snapshot.val().yellowHousePoints;
     }, function(errorObject) {
-        console.log("Read House Points failed: " + errorObject.code);
+    console.log("Read House Points failed: " + errorObject.code);
     });
+ 
 
-    //event listeners for red house
+    //event listeners
+    //Red House
     //award
     $(document).on("click", "#award-red", function awardRed(){
         event.preventDefault();
         var redNumeric = parseInt(redPoints);
         redNumeric += 5;
-        database.ref('housePoints').set({
+        database.ref().set({
             redHousePoints: redNumeric
         });
     });
 
     //deduct
-    $(document).on("click", "#deduct-red", function awardRed(){
+    $(document).on("click", "#deduct-red", function deductRed(){
         event.preventDefault();
         var redNumeric = parseInt(redPoints);
         redNumeric -= 5;
-        database.ref('housePoints').set({
+        database.ref().set({
             redHousePoints: redNumeric
         });
     });
 
-    
+    //Blue House
+    //award
+    $(document).on("click", "#award-blue", function awardBlue(){
+        event.preventDefault();
+        var blueNumeric = parseInt(bluePoints);
+        blueNumeric += 5;
+        database.ref().set({
+            blueHousePoints: blueNumeric
+        });
+    });
+
+    //deduct
+    $(document).on("click", "#deduct-blue", function deductBlue(){
+        event.preventDefault();
+        var blueNumeric = parseInt(bluePoints);
+        blueNumeric -= 5;
+        database.ref().set({
+            blueHousePoints: blueNumeric
+        });
+    });
+
+    //Green House
+    //award
+    $(document).on("click", "#award-green", function awardGreen(){
+        event.preventDefault();
+        var greenNumeric = parseInt(greenPoints);
+        greenNumeric += 5;
+        database.ref().set({
+            greenHousePoints: greenNumeric
+        });
+    });
+
+    //deduct
+    $(document).on("click", "#deduct-green", function deductGreen(){
+        event.preventDefault();
+        var greenNumeric = parseInt(greenPoints);
+        greenNumeric -= 5;
+        database.ref().set({
+            greenHousePoints: greenNumeric
+        });
+    });
+
+    //Yellow House
+    //award
+    $(document).on("click", "#award-yellow", function awardYellow(){
+        event.preventDefault();
+        var yellowNumeric = parseInt(yellowPoints);
+        yellowNumeric += 5;
+        database.ref().set({
+            yellowHousePoints: yellowNumeric
+        });
+    });
+
+    //deduct
+    $(document).on("click", "#deduct-yellow", function deductYellow(){
+        event.preventDefault();
+        var yellowNumeric = parseInt(yellowPoints);
+        yellowNumeric -= 5;
+        database.ref().set({
+            yellowHousePoints: yellowNumeric
+        });
+    });
   });
